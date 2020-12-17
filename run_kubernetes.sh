492e75792ee9bcf68f86509d2b996a573b2fdc2c
EC2 Default User <ec2-user@ip-172-31-22-168.us-west-2.compute.internal>
2020-12-17 21:49:57 +0000

    docker and kubernetes sh files populated

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=daydin41/udacity_devops_p4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlpredict --image=$dockerpath --port=80 --labels app=mlpredict
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward mlpredict 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=daydin41/udacity_devops_p4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u daydin41
+docker tag api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
